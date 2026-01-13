850d2dc9ddb10dbe25637dd762fd93043f8cdf36
SidhsHub <87430265+SidhsHub@users.noreply.github.com>
2026-01-13 12:38:51 +0530

    Update 4 fixed rolling window.sql

--- 4 fixed rolling window.sql
+++ 4 fixed rolling window.sql
@@ -1,3 +1,11 @@
+"sales_month"	"naics_code"	"kind_of_business"			"reason_for_null"	"sales"
+"1992-01-01"	"44811"			"Men's clothing stores"							701
+"1992-01-01"	"44812"			"Women's clothing stores"						1873
+"1992-02-01"	"44811"			"Men's clothing stores"							658
+"1992-02-01"	"44812"			"Women's clothing stores"						1991
+"1992-03-01"	"44811"			"Men's clothing stores"							731
+
+
 SELECT
 	SALES_MONTH,
 	sales,
@@ -19,4 +27,27 @@ SELECT
 FROM
 	RETAIL_SALES
 WHERE
+
 	KIND_OF_BUSINESS = 'Women''s clothing stores'
+
+-- OR
+
+SELECT
+	A.sales_month,
+	A.sales,
+	count(B.sales) as count_months_in_ltm,
+	round(avg(B.sales), 2) as avg_ltm,
+	max(B.sales) as max_ltm
+FROM
+	RETAIL_SALES A
+	JOIN RETAIL_SALES B ON B.SALES_MONTH between A.SALES_MONTH - INTERVAL '11 months' and A.SALES_MONTH
+	AND B.KIND_OF_BUSINESS = 'Women''s clothing stores'
+WHERE
+	A.KIND_OF_BUSINESS = 'Women''s clothing stores' group by 1, 2 order by 1
+
+"sales_month"	"sales"	"count_months_in_ltm"	"avg_of_ltm"	"max_of_ltm"
+"1992-01-01"	1873	1						1873.00			1873
+"1992-02-01"	1991	2						1932.00			1991
+"1992-03-01"	2403	3						2089.00			2403
+"1992-04-01"	2665	4						2233.00			2665
+"1992-05-01"	2752	5						2336.80			2752
